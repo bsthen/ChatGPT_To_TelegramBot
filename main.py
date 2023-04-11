0981c10f25f760c04dd415e85a6512a90ca7e284
BAN Sothen <ban.sothen2016@rupp.edu.kh>
2023-04-11 11:23:46 +0700

    Update model from 3 to 3.5

--- main.py
+++ main.py
@@ -1,64 +1,40 @@
 '''
-OpenAI ChatGPT-3 To TelegramBot
+OpenAI ChatGPT-3.5 To TelegramBot
 '''
 
 
 import os
-import openai as ai
+import openai
 from telebot import TeleBot
 from dotenv import load_dotenv
-import time
 
 load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-ai.api_key = os.getenv("OPENAI_API_KEY")
-
-def generate_gpt3_response(user_text, print_output=False):
-    """
-    Query OpenAI GPT-3 for the specific key and get back a response
-    :type user_text: str the user's text to query for
-    :type print_output: boolean whether or not to print the raw output JSON
-    """
-    completions = ai.Completion.create(
-        engine='text-davinci-003',  # Determines the quality, speed, and cost.
-        temperature=0.5,            # Level of creativity in the response
-        prompt=user_text,           # What the user typed in
-        max_tokens=1000,            # Maximum tokens in the prompt AND response
-        n=1,                        # The number of completions to generate
-        stop=None,                  # An optional setting to control response generation
+def chatgpt_response(user_message):
+    completion = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [{
+            "role":"user",
+            "content":user_message
+        }]
     )
+    return completion.choices[0].message['content']
 
-    # Displaying the output can be helpful if things go wrong
-    if print_output:
-        print(completions)
-
-    # Return the first choice's text
-    return completions.choices[0].text
-
-## Path: Telegram
 app = TeleBot(__name__)
 
 @app.route('(?!/).+')
+
 def chatgpt(message):
     '''
-    Function to handle telegram messages
+    Function to handle the chatgpt bot
     '''
     chat_dest = message['chat']['id']
-    print("ID is: ",message['chat']['id']) # Print the chat ID Of Telegram User
-    print("Msg sent: ",message['text']) # Print the message sent by the user
-    ## rety if connect to chatgpt is failed
-    while True:
-        try:
-            user_msg = generate_gpt3_response(message['text'])
-            break
-        except:
-            print("Connection to ChatGPT Failed")
-            ## retry after 5 seconds
-            time.sleep(5)
-            continue
-        
-    print("Bot Respone: ",user_msg) # Print the response of the ChatGPT
-    msg = f"ChatGPT: {user_msg}"
+    print("ID is: ",message['chat']['id'])
+    print("Msg sent: ",message['text'])
+    user_msg = chatgpt_response(message['text'])
+    print("Bot Respone: ",user_msg)
+    msg = f"ChatGPT🤖:  {user_msg}"
     app.send_message(chat_dest, msg)
 
 if __name__ == '__main__':
